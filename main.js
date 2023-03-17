ccfcaa736118bc79fc6091cfa5d2bae7b5268616
WAtuchei <112400074+WAtuchei@users.noreply.github.com>
2023-03-17 20:25:04 +0700

    Squashed commit from WebTest branch:
    commit 732e9bd1ddb459ce0138bcf3fafcc64b5ef64a5c
    Author: WAtuchei <112400074+WAtuchei@users.noreply.github.com>
    Date:   Fri Mar 17 20:09:07 2023 +0700
    
        Fix null
    
    commit 992bb691fde8ebc2f32f275abe544a4e387f8c07
    Author: WAtuchei <112400074+WAtuchei@users.noreply.github.com>
    Date:   Fri Mar 17 17:31:14 2023 +0700
    
        Fix Link
    
    commit a8fe579ecf58a4ad00a242c560f3cbcb709bb6c1
    Author: WAtuchei <112400074+WAtuchei@users.noreply.github.com>
    Date:   Fri Mar 17 17:18:42 2023 +0700
    
        Finish validated function
    
    commit 50d8ed3bd90fc7e7b5e475dd3487211902e574a8
    Author: WAtuchei <112400074+WAtuchei@users.noreply.github.com>
    Date:   Fri Mar 17 01:00:38 2023 +0700
    
        Add basic function and animation
    
    commit 0488889ec56b671a78287eb25125ff7be0fe5dee
    Author: WAtuchei <112400074+WAtuchei@users.noreply.github.com>
    Date:   Wed Mar 15 13:48:57 2023 +0700
    
        Setup for Test

---
+++ main.js
@@ -0,0 +1,69 @@
+"use strict";
+const emailForm = document.getElementById("emailForm");
+const userEmail = document.getElementById("userEmail");
+const errorIcon = document.querySelector(".errorIcon");
+const errorMessage = document.querySelector(".errorMessage");
+const errorSet = [errorIcon, errorMessage];
+
+// Email Check
+const emailChecked = (input) => {
+    const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    const testEmail = input.value.trim();
+
+    if (re.test(testEmail)) {
+        const subEmail = testEmail.toLowerCase();
+
+        localStorage.setItem('subEmail', subEmail);
+        return true
+
+    } else {
+        invalidForm();
+    }
+}
+// Length Check
+const lengthChecked = (input, min) => {
+    const testLength = input.value.trim();
+    
+    if (testLength.length === 0) {
+        invalidForm();
+
+    } else if (testLength.length < min) {
+        errorMessage.innerText = "The email was too short";
+
+    } else {
+        return true
+    }
+}
+// Error
+const invalidForm = () => {
+    errorSet.map((error) => {
+        error.style.display = "block";
+    })
+    userEmail.classList.add('input-invalid');
+    errorMessage.classList.add('fading-animated')
+    errorMessage.innerText = "Please provide a valid email";
+
+    // after CSS animated
+    errorMessage.addEventListener('animationend', () => {
+        errorMessage.classList.remove('fading-animated')
+    })
+}
+// Submit
+emailForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = emailChecked(userEmail);
+    const maillength = lengthChecked(userEmail, 6)
+
+    switch (email && maillength) {
+        case true:
+            window.location.href = "https://watuchei.github.io/-Custom-FrontEndMentor-JS1-BaseApparel/subscribed.html";
+            break;
+        default:
+            break;
+    }
+})  
+// /[^@\s]+@[^@\s]+\.[^@\s]+/
+// /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+//  /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/
+// /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+// /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
---
+++ sub.js
@@ -0,0 +1,15 @@
+"use strict";
+const subMessage = document.getElementById('subMessage');
+const sessionEmail = localStorage.getItem('subEmail');
+
+window.addEventListener('load', () => {
+    if (sessionEmail === null) {
+        subMessage.innerHTML = `We'll notice you by the email.`
+    } else {
+        subMessage.innerHTML = `We'll notice you by the email <span>${sessionEmail}</span>`
+    }
+ })
+ window.addEventListener('unload', () => {
+    localStorage.clear();
+    window.location.href = "https://watuchei.github.io/-Custom-FrontEndMentor-JS1-BaseApparel/";
+})
